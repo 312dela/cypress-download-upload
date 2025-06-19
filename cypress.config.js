7650a51027c6f0298f0ee2d80474f4b98d010fc3
“dela” <312dela@gmail.com>
2025-06-19 15:31:48 +0700

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("cypress");
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs');
+
+function setupNodeEvents(on, config) {
+  // implement node event listeners here
+  require('cypress-mochawesome-reporter/plugin')(on);
+  on('task', {
+    excelToJsonConverter(filePath) {
+      const result = excelToJson({
+        source: fs.readFileSync(filePath) // fs.readFileSync return a Buffer
+      });
+      return result
+    }
+  })
+
+
+}
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    video: true,
+    setupNodeEvents,
+    specPattern: 'cypress/integration/examples/*.js'
+  },
+});
---
+++ cypress/integration/examples/DownloadUpload.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress"/>
+
+describe('Download Upload Validation', function () {
+    it('Verify Downloaded File', function () {
+        const fruitName = 'Banana' 
+        cy.visit('https://rahulshettyacademy.com/upload-download-test/index.html')
+        cy.get('#downloadButton').click()
+        cy.wait(2000)
+        const filePath = Cypress.config("fileServerFolder") + "/cypress/downloads/download.xlsx"
+        cy.task('excelToJsonConverter', filePath).then(function (result) {
+            // cy.log(JSON.stringify(result));
+            expect(fruitName).to.equal(result.Sheet1[4].B)            
+        })
+    })
+})
