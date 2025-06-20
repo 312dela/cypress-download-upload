b2fdd1c0c54afd0b6dd8d3af6f659714fa4e074c
“dela” <312dela@gmail.com>
2025-06-20 12:42:03 +0700

    test trigger 6

--- cypress.config.js
+++ cypress.config.js
@@ -3,34 +3,28 @@ const excelToJson = require('convert-excel-to-json');
 const fs = require('fs');
 
 function setupNodeEvents(on, config) {
-  // implement node event listeners here
   require('cypress-mochawesome-reporter/plugin')(on);
   on('task', {
     excelToJsonConverter(filePath) {
       const result = excelToJson({
-        source: fs.readFileSync(filePath) // fs.readFileSync return a Buffer
+        source: fs.readFileSync(filePath)
       });
-      return result
+      return result;
     }
-  })
-
-
+  });
 }
+
 module.exports = defineConfig({
-
-
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reportDir: 'cypress/reports/json',
-    overwrite: false,
-    html: false,
+    reportDir: "cypress/reports/html",
+    overwrite: true,      
+    html: true,
     json: true
   },
-
-
   e2e: {
     video: true,
     setupNodeEvents,
-    specPattern: 'cypress/integration/examples/*.js'
+    specPattern: 'cypress/integration/examples/*.js',
   },
 });
