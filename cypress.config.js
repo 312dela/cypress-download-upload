c630dd034746d17806336e1eeba3937ebcb585b6
“dela” <312dela@gmail.com>
2025-06-30 11:15:47 +0700

    Initial commit

---
+++ cypress.config.js
@@ -0,0 +1,88 @@
+const { defineConfig } = require("cypress");
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs');
+const ExcelJs = require('exceljs');
+
+function setupNodeEvents(on, config) {
+  require('cypress-mochawesome-reporter/plugin')(on);
+
+  on('task', {
+    excelToJsonConverter(filePath) {
+      const result = excelToJson({
+        source: fs.readFileSync(filePath)
+      });
+      return result;
+    }
+  });
+
+ on('task', {
+  async writeExcelTest({ searchText, replaceText, change, fileLocation, appendRow }) {
+    const workbook = new ExcelJs.Workbook();
+    await workbook.xlsx.readFile(fileLocation);
+    const worksheet = workbook.getWorksheet('Sheet1');
+
+    if (searchText) {
+      const output = await readExcel(worksheet, searchText);
+      if (output.row !== -1 && output.column !== -1) {
+        const cell = worksheet.getCell(output.row, output.column + change.colChange);
+        cell.value = replaceText;
+      }
+    }
+
+    if (appendRow) {
+      const lastRow = worksheet.lastRow;
+      const newRowIndex = lastRow.number + 1;
+      worksheet.getRow(newRowIndex).values = appendRow;
+    }
+
+    return workbook.xlsx.writeFile(fileLocation)
+      .then(() => true)
+      .catch(() => false);
+  }
+})
+
+
+  //
+
+
+
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
+async function readExcel(worksheet,searchText)
+{
+    let output = {row:-1,column:-1};
+    worksheet.eachRow((row,rowNumber) =>
+    {
+          row.eachCell((cell,colNumber) =>
+          {
+              if(cell.value === searchText)
+              {
+                  output.row=rowNumber;
+                  output.column=colNumber;
+              }
+  
+  
+          }  )
+    
+    })
+    return output;
+}
+
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: "cypress/reports/html",
+    overwrite: true,
+    html: true,
+    json: true
+  },
+  e2e: {
+    video: true,
+    setupNodeEvents,
+    specPattern: 'cypress/e2e/*.js',
+  },
+});
---
+++ cypress/e2e/DownloadUpload.js
@@ -0,0 +1,54 @@
+/// <reference types="cypress"/>
+
+describe('Excel Data Validation', () => {
+    let fruit;
+    let fruitChanges;
+    const fileLocation = Cypress.config('fileServerFolder') + '/cypress/downloads/download.xlsx';
+
+    before(() => {
+        cy.fixture('testData').then((data) => {
+            fruit = data.searchFruit;
+            fruitChanges = data.modifyFruit;
+        })
+    })
+
+    beforeEach(() => {
+        cy.visit('https://rahulshettyacademy.com/upload-download-test/index.html');
+        cy.get('#downloadButton').click();
+        cy.wait(2000);
+    })
+
+    it('Verify Downloaded File Data', () => {
+        cy.task('excelToJsonConverter', fileLocation).then((result) => {
+            // cy.log(JSON.stringify(result));
+            expect(fruit.fruitName1).to.equal(result.Sheet1[4].B);
+        })
+    })
+
+    it("verify excel update and append", () => {
+        const replaceNum = 450;
+        const searchTextFruit = "Mango";
+
+        const newFruitRow = [
+            7,           // S No (karena sekarang sudah ada sampai 6, maka jadi 7)
+            "Jambu",     // Fruit Name
+            "Pink",      // Color
+            "200",          // Price (kosong)
+            "All"           // Season (kosong)
+        ];
+
+        cy.task('writeExcelTest', {
+            searchText: searchTextFruit,
+            replaceText: replaceNum,
+            change: { rowChange: 0, colChange: 2 },
+            fileLocation,
+            appendRow: newFruitRow
+        });
+        cy.get("#fileinput").selectFile(fileLocation);
+        cy.contains(searchTextFruit).parent().parent().find("#cell-4-undefined")
+            .should('have.text', replaceNum);
+    });
+
+
+
+})
